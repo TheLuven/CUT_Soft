1c950dd48bd957388b77f0bd2f3554ae10b65a4d
TheLuven <venuleth.dev@gmail.com>
2023-12-24 15:44:01 +0100

    Change database to add new function such as : SimpleSelect and Update Queries And a get actor by ID.

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -1,5 +1,7 @@
 package database;
 
+import actors.Actor;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,35 +9,66 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
     DatabaseConnection database;
-    public DatabaseManager(DatabaseConnection database){
-        this.database=database;
+
+    public DatabaseManager(DatabaseConnection database) {
+        this.database = database;
     }
 
+    public void simpleUpdateQuery(int id, String value, String tableName, String columnName) {
+        try {
+            Connection connection = this.database.getConnection();
+            String updateQuery = "UPDATE "+tableName+" SET "+columnName+" = ? WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+            preparedStatement.setString(1, value);
+            preparedStatement.setInt(2, id);
+            System.out.println("[DEBUG] Update Query : "+preparedStatement.toString());
+            // Execute the update query
+            int rowsAffected = preparedStatement.executeUpdate();
+            // Check the number of rows affected
+            System.out.println("[DEBUG] "+rowsAffected + " row(s) updated successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public void getActorByID(){
-        try (Connection connection = this.database.getConnection()) {
-            String selectQuery = "SELECT * FROM person";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
-                // Execute the query and get the result set
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    // Process the result set
-                    while (resultSet.next()) {
-                        // Retrieve data from the result set
-                        int id = resultSet.getInt("id");
-                        String name = resultSet.getString("surname");
-                        // ... add more columns as needed
+    public String simpleGetQuery(int id, String value, String tableName) {
+        try {
+            Connection connection = this.database.getConnection();
+            String updateQuery = "SELECT ? FROM ? WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+            preparedStatement.setString(1, value);
+            preparedStatement.setString(2, tableName);
+            preparedStatement.setInt(3, id);
+            System.out.println("[DEBUG] Get Query : "+preparedStatement.toString());
+            // Execute the get query
+            ResultSet resultSet = preparedStatement.executeQuery();
+            // Check the number of rows affected
+            return resultSet.getString(value);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 
-                        // Process the retrieved data
-                        System.out.println("ID: " + id + ", Name: " + name);
-                    }
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }catch (Exception e){
-
+    public Actor getActorByID(int id) {
+        try {
+            Connection connection = this.database.getConnection();
+            String selectQuery = "SELECT name,surname,email,gender,role,className,description FROM person WHERE id=" + id;
+            PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
+            // Execute the query and get the result
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            String name = resultSet.getString("name");
+            String surname = resultSet.getString("surname");
+            String email = resultSet.getString("email");
+            String gender = resultSet.getString("gender");
+            String description = resultSet.getString("description");
+            String role = resultSet.getString("role");
+            String className = resultSet.getString("className");
+            return new Actor(this, id, name, surname, role, email, description, className, gender);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
