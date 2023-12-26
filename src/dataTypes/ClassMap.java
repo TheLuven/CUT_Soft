108761556bac05f5da420102612a22ab65f09b17
TheLuven <venuleth.dev@gmail.com>
2023-12-26 22:33:17 +0100

    Add getter and setters of classmap.java

--- src/dataTypes/ClassMap.java
+++ src/dataTypes/ClassMap.java
@@ -1,17 +1,50 @@
 package dataTypes;
 
-import dataTypes.actors.Class;
-import dataTypes.actors.Subject;
 import dataTypes.actors.Teacher;
 
 public class ClassMap {
     private Teacher teacher;
     private Subject subject;
     private Class aClass;
+
+    private String status;
     public ClassMap(Teacher teacher,Subject subject,Class aClass){
         this.teacher=teacher;
         this.subject=subject;
         this.aClass=aClass;
+        this.status = "undefined";
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Class getaClass() {
+        return aClass;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setaClass(Class aClass) {
+        this.aClass = aClass;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
     }
 
     @Override
