bf51db9755933ba059e09f7ebcda60360add728f
TheLuven <venuleth.dev@gmail.com>
2023-12-28 15:24:57 +0100

    Created the class that represent the editing of classMap

---
+++ src/dataTypes/classMap/ClassMapLayer.java
@@ -0,0 +1,11 @@
+package dataTypes.classMap;
+
+public class ClassMapLayer {
+    String name;
+    public ClassMapLayer(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
